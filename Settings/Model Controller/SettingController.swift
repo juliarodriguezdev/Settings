7a3b97d2cb8baacae7fd4af105709e6a8da9ac0d
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-17 11:23:44 -0600

    Created model, model controller files

--- Settings/Model Controller/SettingController.swift
+++ Settings/Model Controller/SettingController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2019 julia rodriguez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SettingController {
+    
+    // Singleton
+    static let sharedInstance = SettingController()
+    
+    let settings: [Setting] = {
+        
+        let music = Setting(name: "iTunes", icon: #imageLiteral(resourceName: "iTunes"), isOn: false)
+        let appStore = Setting(name: "App Store", icon: #imageLiteral(resourceName: "App Store"), isOn: false)
+        let iBooks = Setting(name: "iBooks", icon: #imageLiteral(resourceName: "iBooks"), isOn: true)
+        return[music, appStore, iBooks]
+    }()
+    
+    func toggleIsOn(for setting: Setting) {
+        setting.isOn = !setting.isOn
+        
+    }
+    
+}
