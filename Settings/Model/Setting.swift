c2e23650c2b80e757e74b736ea9d56dba94c6cd5
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-17 10:59:44 -0600

    Completed model

--- Settings/Model/Setting.swift
+++ Settings/Model/Setting.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2019 julia rodriguez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Setting {
+    
+    let name: String
+    let icon: UIImage
+    var isOn: Bool
+    
+    init(name: String, icon: UIImage, isOn: Bool) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
