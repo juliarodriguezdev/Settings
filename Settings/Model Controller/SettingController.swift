13477eba049dee1d559d0b0b9901eb9613329284
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-17 10:53:52 -0600

    Set up Storyboard and file organization

---
+++ Settings/Model Controller/SettingController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingController.swift
+//  Settings
+//
+//  Created by julia rodriguez on 6/17/19.
+//  Copyright © 2019 julia rodriguez. All rights reserved.
+//
+
+import Foundation
---
+++ Settings/Model/Setting.swift
@@ -0,0 +1,9 @@
+//
+//  Setting.swift
+//  Settings
+//
+//  Created by julia rodriguez on 6/17/19.
+//  Copyright © 2019 julia rodriguez. All rights reserved.
+//
+
+import Foundation
